a00d1c30683624d63fdf1e16cd37481769859127
Osman <osmanorucc@gmail.com>
2020-11-21 18:07:28 +0300

    Added template and something edited

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,12 +16,24 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+
+    <link href="{{ asset('assets/css/argon.css?v=1.2.0') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/nucleo/css/nucleo.css') }}" rel="stylesheet">
+
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        @guest
+            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Task Management') }}
@@ -31,11 +43,6 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
@@ -97,10 +104,104 @@
                 </div>
             </div>
         </nav>
+        @else
+            <nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+                <div class="scrollbar-inner">
+                    <!-- Brand -->
+                    <div class="sidenav-header  align-items-center">
+                        <a class="navbar-brand" href="javascript:void(0)">
+                            <img src="{{asset('assets/img/brand/blue.png')}}" class="navbar-brand-img" alt="...">
+                        </a>
+                    </div>
+                    <div class="navbar-inner">
+                        <!-- Collapse -->
+                        <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+                            <!-- Nav items -->
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="{{route('home')}}">
+                                        <i class="ni ni-tv-2 text-primary"></i>
+                                        <span class="nav-link-text">Dashboard</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{route('user.index')}}">
+                                        <i class="ni ni-single-02 text-yellow"></i>
+                                        <span class="nav-link-text">All User</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{route('task.index')}}">
+                                        <i class="ni ni-bullet-list-67 text-default"></i>
+                                        <span class="nav-link-text">All Task</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{route('notification.index')}}">
+                                        <i class="ni ni-send text-dark"></i>
+                                        <span class="nav-link-text">All Notification</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        <i class="ni ni-key-25 text-info"></i>
+                                        <span class="nav-link-text">Logout</span>
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </li>
+                            </ul>
+                            <!-- Divider -->
+                            <hr class="my-3">
+                            <!-- Heading -->
+                            <h6 class="navbar-heading p-0 text-muted">
+                                <span class="docs-normal">Generate Something</span>
+                            </h6>
+                            <!-- Navigation -->
+                            <ul class="navbar-nav mb-md-3">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{route('user.create')}}">
+                                        <i class="ni ni-single-02 text-yellow"></i>
+                                        <span class="nav-link-text">Generate User</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{route('task.create')}}">
+                                        <i class="ni ni-palette"></i>
+                                        <span class="nav-link-text">Define Task</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{route('notification.create')}}">
+                                        <i class="ni ni-ui-04"></i>
+                                        <span class="nav-link-text">Generate Notification</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        @endguest
 
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+
+
+    <script src="{{asset('assets/vendor/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/js-cookie/js.cookie.js')}}"></script>
+    <script src="{{asset('assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js')}}"></script>
+    <!-- Optional JS -->
+    <script src="{{asset('assets/vendor/chart.js/dist/Chart.min.js')}}"></script>
+    <script src="{{'vendor/chart.js/dist/Chart.extension.js'}}"></script>
+    <!-- Argon JS -->
+    <script src="{{asset('assets/js/argon.js?v=1.2.0')}}"></script>
 </body>
 </html>
